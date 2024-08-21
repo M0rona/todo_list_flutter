1eed5a473cb2726b2c2a3e797ebcc188a9e8f251
M0rona <gabrielmorona0229@gmail.com>
2024-08-21 16:43:56 -0300

    feat: :building_construction: building repository and service

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,7 +1,11 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:todo_list_provider/app/app_widget.dart';
 import 'package:todo_list_provider/app/core/database/sqlite_connection_factory.dart';
+import 'package:todo_list_provider/app/repositories/user/user_repository_impl.dart';
+import 'package:todo_list_provider/app/services/user/user_service.dart';
+import 'package:todo_list_provider/app/services/user/user_service_impl.dart';
 
 class AppModule extends StatelessWidget {
   const AppModule({super.key});
@@ -10,9 +14,16 @@ class AppModule extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
+        Provider(create: (_) => FirebaseAuth.instance),
         Provider(
           create: (_) => SqliteConnectionFactory(),
           lazy: false,
+        ),
+        Provider<UserRepositoryImpl>(
+          create: (context) => UserRepositoryImpl(auth: context.read()),
+        ),
+        Provider<UserService>(
+          create: (context) => UserServiceImpl(userRepository: context.read()),
         )
       ],
       child: const AppWidget(),
---
+++ lib/app/repositories/user/user_repository_impl.dart
@@ -0,0 +1,29 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:todo_list_provider/app/exception/auth_exception.dart';
+import 'package:todo_list_provider/app/repositories/user/user_repository.dart';
+
+class UserRepositoryImpl implements UserRepository {
+  final FirebaseAuth _auth;
+
+  UserRepositoryImpl({required FirebaseAuth auth}) : _auth = auth;
+
+  @override
+  Future<User?> register(String email, String password) async {
+    try {
+      final userCredential = await _auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+      return userCredential.user;
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'email-already-in-use') {
+        final loginTypes = await _auth.fetchSignInMethodsForEmail(email);
+        if (loginTypes.contains('password')) {
+          throw AuthException(message: 'Email já cadastrado');
+        } else {
+          throw AuthException(message: 'Email já cadastrado pelo google!');
+        }
+      } else {
+        throw AuthException(message: e.message ?? 'Erro ao registrar usuário');
+      }
+    }
+  }
+}
