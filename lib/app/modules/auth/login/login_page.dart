06206ceb6f43eb5497c12c6920945774ae1a9e6d
M0rona <gabrielmorona0229@gmail.com>
2024-08-21 15:59:19 -0300

    feat: :wrench: configured firebase for the project

--- lib/app/modules/auth/login/login_page.dart
+++ lib/app/modules/auth/login/login_page.dart
@@ -87,7 +87,9 @@ class LoginPage extends StatelessWidget {
                             children: [
                               const Text('Não tem uma conta?'),
                               TextButton(
-                                onPressed: () {},
+                                onPressed: () {
+                                  Navigator.of(context).pushNamed('/register');
+                                },
                                 child: const Text('Cadastre-se'),
                               )
                             ],
