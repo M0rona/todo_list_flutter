20cfb93537e4b4a989879ea3a65a18ee8280b8cd
M0rona <gabrielmorona0229@gmail.com>
2024-08-26 15:41:46 -0300

    refactor: :fire: Removed unnecessary import

--- lib/app/repositories/user/user_repository_impl.dart
+++ lib/app/repositories/user/user_repository_impl.dart
@@ -1,5 +1,4 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/services.dart';
 import 'package:todo_list_provider/app/exception/auth_exception.dart';
 import 'package:todo_list_provider/app/repositories/user/user_repository.dart';
